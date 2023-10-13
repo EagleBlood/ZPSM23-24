b1780b749ab536aa9038ceb31c9d2c1a3d6c9abd
EagleBlood <35221@student.pwsztar.edu.pl>
2023-10-13 10:27:51 +0200

    zad2

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 const lod = require('lodash');
 
-console.log("hello");
+let tab1 = [1, 2, 3, 4, 5];
+
+let max = tab1[0];
+let min = tab1[0];
+
+for (let i = 1; i < tab1.length; i++) {
+    if (tab1[i] > max) {
+        max = tab1[i];
+    }
+    if (tab1[i] < min) {
+        min = tab1[i];
+    }
+}
+
+console.log("The largest element is: " + max);
+console.log("The smallest element is: " + min);
