28e9484bbc721cbc58d63cef2ec3897c70104175
EagleBlood <35221@student.pwsztar.edu.pl>
2023-12-08 11:27:19 +0100

    api results

--- screens/Results.tsx
+++ screens/Results.tsx
@@ -1,12 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, SafeAreaView, ScrollView, TouchableOpacity, RefreshControl, FlatList } from 'react-native';
 import { styles } from '../styles/styles';
 import { stylesResults } from '../styles/stylesResults';
-import resultsData from "../data/resultsData"
+
+interface ResultItem {
+  id: number;
+  nick: string;
+  score: number;
+  total: number;
+  type: string;
+  createdOn: string;
+}
 
 const ResultsView: React.FC = () => {
-  const [selectedMenu, setSelectedMenu] = useState('Top 10');
+  const [selectedMenu, setSelectedMenu] = useState('Top 5');
   const [refreshing, setRefreshing] = useState(false);
+  const [resultsData, setResultsData] = useState<ResultItem[]>([]);
+  const [topTenData, setTopTenData] = useState<ResultItem[]>([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://tgryl.pl/quiz/results');
+      const data: ResultItem[] = await response.json();
+      setResultsData(data);
+      const aggregatedData = aggregateData(data);
+      setTopTenData(aggregatedData.slice(0, 5));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const aggregateData = (data: ResultItem[]): ResultItem[] => {
+    const userScores: { [key: string]: ResultItem } = {};
+
+    data.forEach(item => {
+      if (userScores[item.nick]) {
+        userScores[item.nick].score += item.score;
+        userScores[item.nick].total += item.total;
+      } else {
+        userScores[item.nick] = { ...item };
+      }
+    });
+
+    const aggregatedData: ResultItem[] = Object.values(userScores);
+    aggregatedData.sort((a, b) => b.score - a.score);
+
+    return aggregatedData;
+  };
 
   const handleMenuClick = (menu: string) => {
     setSelectedMenu(menu);
@@ -14,17 +58,8 @@ const ResultsView: React.FC = () => {
 
   const onRefresh = React.useCallback(() => {
     setRefreshing(true);
-    setTimeout(() => setRefreshing(false), 1000);
+    fetchData().then(() => setRefreshing(false));
   }, []);
-
-  interface ResultItem {
-    id: number;
-    nick: string;
-    score: number;
-    total: number;
-    type: string;
-    date: string;
-  }
 
   const renderItem = ({ item }: { item: ResultItem }) => (
     <View style={stylesResults.resultsBox}>
@@ -32,7 +67,7 @@ const ResultsView: React.FC = () => {
         <View style={stylesResults.resultsBoxItems}>
           <Text style={stylesResults.resultsBoxText}>{item.nick}</Text>               
           <Text style={stylesResults.resultsBoxTypeText}>{item.type}</Text>
-          <Text style={stylesResults.resultsBoxText}>{item.date}</Text>
+          <Text style={stylesResults.resultsBoxText}>{item.createdOn}</Text>
         </View>
         <View style={stylesResults.resultsBoxItems}>
           <Text style={stylesResults.resultsBoxScoreText}>{item.score}</Text>
@@ -45,7 +80,7 @@ const ResultsView: React.FC = () => {
   return (
     <SafeAreaView style={styles.body}>
       <FlatList
-        data={resultsData.results}
+        data={selectedMenu === 'Top 5' ? topTenData : aggregateData(resultsData)}
         renderItem={renderItem}
         keyExtractor={item => item.id.toString()}
         refreshControl={
@@ -61,9 +96,9 @@ const ResultsView: React.FC = () => {
                 <Text style={stylesResults.resultsBoxHeaderText}>Top User's Results</Text>
   
                 <View style={stylesResults.resultsBottomMenuItems}>
-                  <TouchableOpacity onPress={() => handleMenuClick('Top 10')}>
-                    <Text style={[stylesResults.resultsBottomMenuText, selectedMenu === 'Top 10' && stylesResults.selectedMenu]}>
-                      Top 10
+                  <TouchableOpacity onPress={() => handleMenuClick('Top 5')}>
+                    <Text style={[stylesResults.resultsBottomMenuText, selectedMenu === 'Top 5' && stylesResults.selectedMenu]}>
+                      Top 5
                     </Text>
                   </TouchableOpacity>
                   <TouchableOpacity onPress={() => handleMenuClick('All the time')}>
@@ -84,7 +119,6 @@ const ResultsView: React.FC = () => {
       />
     </SafeAreaView>
   );
-
 }
 
 export default ResultsView;
